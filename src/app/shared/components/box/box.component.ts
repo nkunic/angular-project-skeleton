6ba583a4f699fa3f29fea9adc1bf7063ac4a628e
nkunic <natasa.kunic@gmail.com>
2022-03-27 14:40:11 +0200

    1 file modified, 6 added

---
+++ src/app/shared/components/box/box.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.scss'],
+})
+export class BoxComponent implements OnInit {
+  @Input() isActive: boolean;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/shared/components/toggle/toggle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.scss'],
+})
+export class ToggleComponent implements OnInit {
+  isRound = true;
+  isChecked = true;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
